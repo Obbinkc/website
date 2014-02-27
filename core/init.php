9ba35246ef49389af31effafa5cd7db988cb5d21
superzaky <superzaky@hotmail.com>
2014-02-27 01:38:04 +0100

    Courses geadd aan courses.php

--- core/init.php
+++ core/init.php
@@ -2,9 +2,9 @@
 session_start ();
 //error_reporting(0);
 
-require ('/../core/database/connect.php');
-require ('/../core/functions/general.php');
-require ('/../core/functions/users.php');
+require ('core/database/connect.php');
+require ('core/functions/general.php');
+require ('core/functions/users.php');
 
 if (logged_in() == true) {
 	$session_user_id = $_SESSION['user_id'];
--- courses.php
+++ courses.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
@@ -17,24 +16,51 @@ require ('includes/header.php');
         <button type="submit" class="btn btn-default">Submit</button>
     </form>
     <div id="leftside_menu">
-    <div class="col-md-4 col-lg-offset-0">
-        <ul class="nav nav-pills nav-stacked">
-            <li><a href="#">Jaar 1</a></li>
-            <li><a href="#">Jaar 2</a></li>
-            <li><a href="#">Jaar 3</a></li>
-            <li><a href="#">Jaar 4</a></li>
-        </ul> 
-    </div>
+        <div class="col-md-4 col-lg-offset-0">
+            <ul class="nav nav-pills nav-stacked">
+                <li><a href="#">Jaar 1</a></li>
+                <li><a href="#">Jaar 2</a></li>
+                <li><a href="#">Jaar 3</a></li>
+                <li><a href="#">Jaar 4</a></li>
+            </ul> 
         </div>
-    <?php
+    </div>
+   
 
+        <table class="table table-hover">
+            <tr>
+                <td><b>Course id</b></td>
+                <td><b>Course naam</b></td>
+               
+            </tr>
+            <?php
+
+            function fillTable($courseId, $courseName) {
+                $HTML = '';
+                $HTML .= '<tr>';
+                $HTML .= '<td>' . $courseId . '</td>';
+                $HTML .= '<td>' . $courseName . '</a></td>';
+                $HTML .= '</tr>';
+                echo $HTML;
+            }
+
+            $result = mysql_query("SELECT `course_id`,`name` FROM `courses` ");
+            while ($row = mysql_fetch_array($result)) {
+                $courseId = $row['course_id'];
+                $courseName = $row['name'];
+
+                fillTable($courseId, $courseName);
+            }
+            ?>
+        </table>
+   
+    <?php
 } else {
     include ('home.php');
 }
 ?>
-    <div class="container"></div>
+<div class="container"></div>
 
 <?php
-
 require ('includes/footer.php');
 ?>
