b86057383bae47a98903e4f325b9a8fa12b07aa2
Obbinkc <christiaan_obbink@hotmail.com>
2014-04-14 09:09:40 +0200

    loginform
    added an offset to make it center in page

--- loginform.php
+++ loginform.php
@@ -1,7 +1,13 @@
 
-	<form class="form-signin" role="form" action="login.php" method="post">
-		<h2 class="form-signin-heading">Sign In</h2>
-		<input type="text" class="form-control" name="username" placeholder="username" required autofocus>
-		<input type="password" class="form-control" name="password" placeholder="password" required>
-		<button class="btn btn-s btn-primary btn-block" type="submit">Sign in</button>
-	</form>
+<div class="row">
+	<div class="col-xs-4 col-xs-offset-4">
+	 
+		<form class="form-signin" role="form" action="login.php" method="post">
+			<h2 class="form-signin-heading">Sign In</h2>
+			<input type="text" class="form-control" name="username" placeholder="username" required autofocus>
+			<input type="password" class="form-control" name="password" placeholder="password" required>
+			<button class="btn btn-s btn-primary btn-block" type="submit">Sign in</button>
+		</form>
+	 
+	</div>
+</div>	
