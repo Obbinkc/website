7d2d20c33cb48214a3cce5fc029ee485ff3c98c1
Clayver Martina <clayver.martina@outlook.com>
2014-05-21 10:22:58 +0200

    searchresult changes

--- searchresults.php
+++ searchresults.php
@@ -1,6 +1,7 @@
 <?php
 require ('core/init.php');
-require ('includes/header.php');
+//require ('includes/header.php');
+require ('core/functions/courseFunctions.php');
 ?>
 
 
@@ -28,31 +29,62 @@ if (isset($_POST['search'])) {
     $search = $_POST['search'];
 
     /* @var $POST type */
-    $search_sql = "SELECT * FROM courses WHERE name = '" . $search . "'";
-    $search_query = mysql_query($search_sql);
-
-    if (mysql_num_rows($search_query) > 0) {
-        $search_rs = mysql_fetch_assoc($search_query);
+    //SELECT *
+    $search_sql = "SELECT * FROM courses WHERE name OR year = '" . $search . "'";
+    $search_query = mysqli_query(Database::getDatabaseConnection(),$search_sql);
+    
+    //echo 'search_sql '. $search_sql;
+    echo 'search_query '. $search_query;   
+    //$courses = new courseFunctions(); 
+    //$result = $courses->getCoursesByName($name, $year);
+  //  echo 'result '.$result;
+    
+    if (mysqli_num_rows($search_query) > 0) {
+        $search_rs = mysqli_fetch_assoc($search_query);
+         
         do {
-            echo $search_rs['name'] . "<br>";
-        } while ($search_rs = mysql_fetch_assoc($search_query));
-    }
+            while ($row = $search_rs->fetch_assoc()) {
+            extract($row);
+            echo "<tr>";
+            echo "<td>" . $row['course_id'] . "</td>";
+            echo "<td>" . $row['name'] . "</td>";
+            $courseId = $row['course_id'];
+            
+            if ($user_data['type'] == 1) {
+                echo '<td><a onclick="return confirm(\'Delete course? \')" href="deleteCourse.php?id=' . $courseId . '">delete</td>';
+                echo '<td><a href="updateCourse.php?id=' . $courseId . '">update</td>';
+                echo '<td><a href="lessons.php?id=' . $courseId . '">lessons</td>';
+                echo "</tr>";
+                
+                }else if ($user_data['type'] == 0) {
+                            echo '<td><a href="lessons.php?id=' . $courseId . '">lessons</td>';
+                            echo "</tr>";
+                }
+        }
+            //echo $search_rs['name'] . "<br>";
+            
+            }while ($search_rs = mysqli_fetch_assoc($search_query));
+         
+    
+        }else {
+            echo "<h3>No Results</h3>";
+        }
 }
 
 ?>
        
 
 <?php 
-if (mysql_num_rows($search_query) > 0) {
+/*if (mysqli_num_rows($search_query) > 0) {
     do {
         ?>
                 <P><?php echo $search_rs['name'] . "<br>" ; ?></p>   
 
             <?php
-            } while ($search_rs = mysql_fetch_assoc($search_query));
+            } while ($search_rs = mysqli_fetch_assoc($search_query));
         } else {
-            echo "LOL No results found";
-        }
+            //echo "LOL No results found";
+        }*/
         ?>
     </body>
 
